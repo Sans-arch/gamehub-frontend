d3697e899a0d25896caa25135fb9acf0228a8dec
Santiago Fleitas <sansfleitas@gmail.com>
2023-09-30 16:38:04 -0300

    Create styles to Game page

--- src/components/Game/Layout/index.tsx
+++ src/components/Game/Layout/index.tsx
@@ -1,29 +1,60 @@
 import Image from "next/image";
-import { Container, GameInitialInfo, Overlay, Rating, Title } from "./styles";
+import {
+  Container,
+  Content,
+  Description,
+  GameInitialInfo,
+  GameMedia,
+  LogoContainer,
+  MainGameImageContainer,
+  Overlay,
+  Rating,
+  Title,
+} from "./styles";
 import { AiFillStar } from "react-icons/ai";
 
 import logo from "../../../assets/logo/logo-white-removebg-preview.png";
+import fifa from "../../../assets/images/fifaatualizado.png";
 
 export function Layout() {
   return (
     <Container>
       <Overlay>
-        <Image src={logo.src} alt="Logo" width={176} height={181} />
+        <LogoContainer>
+          <Image src={logo.src} alt="Logo" width={176} height={181} />
+        </LogoContainer>
         <GameInitialInfo>
-          <Title>EA Sports FC 24</Title>
-          <p>EA Sports</p>
+          <Content>
+            <Title>EA Sports FC 24</Title>
+            <p>EA Sports</p>
 
-          <Rating>
-            <AiFillStar style={{ color: "#FFD700" }} />
-            <AiFillStar style={{ color: "#FFD700" }} />
-            <AiFillStar style={{ color: "#FFD700" }} />
-            <AiFillStar style={{ color: "#D9D9D9" }} />
-            <AiFillStar style={{ color: "#D9D9D9" }} />
-          </Rating>
+            <Rating>
+              <AiFillStar style={{ color: "#FFD700" }} />
+              <AiFillStar style={{ color: "#FFD700" }} />
+              <AiFillStar style={{ color: "#FFD700" }} />
+              <AiFillStar style={{ color: "#D9D9D9" }} />
+              <AiFillStar style={{ color: "#D9D9D9" }} />
+            </Rating>
 
-          <span>Released On: SEP 29</span>
+            <span>Released On: SEP 29</span>
+          </Content>
         </GameInitialInfo>
       </Overlay>
+
+      <GameMedia>
+        <MainGameImageContainer>
+          <Image src={fifa.src} alt="Logo" width={1120} height={570} />
+        </MainGameImageContainer>
+
+        <Description>
+          <p>
+            EA Sports FC 24 contains more than 30 leagues, featuring over 700
+            teams. It has more than 19,000 players, and 100 stadiums. Familiar
+            competitions confirmed include the Champions League, Premier League
+            (England), Bundesliga (Germany), and La Liga (Spain).
+          </p>
+        </Description>
+      </GameMedia>
     </Container>
   );
 }
--- src/components/Game/Layout/styles.ts
+++ src/components/Game/Layout/styles.ts
@@ -1,35 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 100vh;
   width: 100vw;
-  display: flex;
   background-color: #1c1e21;
 `;
 
 export const Overlay = styled.div`
-  background-color: #be3144;
-  width: 100%;
-  height: 30%;
   display: flex;
   justify-content: space-between;
-  padding: 0 81px;
+
+  background-color: #be3144;
+  width: 100%;
+  height: 35%;
+  box-sizing: border-box;
+  box-shadow: 10px 10px 10px 0px rgba(0, 0, 0, 0.25);
 `;
 
-export const Title = styled.h1`
-  color: #fff;
-  margin: 0;
-  padding: 0;
-  margin-bottom: 10px;
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  padding-left: 60px;
+  padding-bottom: 100px;
 `;
 
 export const GameInitialInfo = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: flex-end;
+  width: 100%;
   font-family: var(--font-poppins);
-
-  height: 100%;
 
   p {
     color: #f5f5f5;
@@ -43,6 +47,23 @@ export const GameInitialInfo = styled.div`
   }
 `;
 
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+
+  height: 100%;
+  width: 40%;
+  padding-right: 80px;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  margin: 0;
+  padding: 0;
+`;
+
 export const Rating = styled.div`
   display: flex;
   margin-top: 44px;
@@ -50,5 +71,38 @@ export const Rating = styled.div`
   svg {
     color: #ffd700;
     font-size: 30px;
+    font-size: 46px;
+  }
+`;
+
+export const MainGameImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+  margin-left: 50px;
+  width: fit-content;
+  bottom: 120px;
+  /* border: 2px solid red; */
+
+  img {
+    border-radius: 30px;
+    box-shadow: 10px 4px 5px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+export const GameMedia = styled.div`
+  display: flex;
+`;
+
+export const Description = styled.div`
+  p {
+    margin: 50px 60px 0 60px;
+    color: #fff;
+    font-family: var(--font-poppins);
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
   }
 `;

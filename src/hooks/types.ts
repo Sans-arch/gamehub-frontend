cfe2217f06b984f5fc5011f5d33485ee17d8fe87
Santiago Fleitas <sansfleitas@gmail.com>
2023-10-30 09:09:34 -0300

    Remove AuthContext
    - Unnecessary 'cause we created the useAuth hook to replace it.

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -3,13 +3,6 @@ export type User = {
   email: string;
 };
 
-export type AuthContextType = {
-  user: User;
-  signIn: (email: string, password: string) => Promise<void>;
-  checkIsAuthenticated: () => Promise<boolean>;
-  logout: () => Promise<void>;
-};
-
 export type SignInResponse = {
   token: string;
   user: User;

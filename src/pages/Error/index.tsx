46fe56eb628a0afaf821c8347be38180a41d06e1
Santiago Fleitas <sansfleitas@gmail.com>
2023-11-12 22:24:15 -0300

    Create Error Page - Not Found

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,17 +1,16 @@
-import { useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
-export default function ErrorPage() {
-  const error = useRouteError() as Error & { statusText?: string };
-  console.error(error);
+import gameHubLogo from '@assets/logo/logo-white-removebg-preview.png';
 
+export default function ErrorPage() {
   return (
     <Container>
+      <Link to="/">
+        <img src={gameHubLogo} alt="Logotipo" width={100} height={100} />
+      </Link>
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <p>Página não encontrada.</p>
     </Container>
   );
 }

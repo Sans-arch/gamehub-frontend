9b87de0a4679268c64edd7ac934dd07afdfe9374
Santiago Fleitas <sansfleitas@gmail.com>
2023-11-15 03:39:46 -0300

    Remove comments

--- src/tests/components/GameCard.test.tsx
+++ src/tests/components/GameCard.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, vi } from 'vitest';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { GameCard, GameCardProps } from '../../components/GameCard';
 import { MemoryRouter } from 'react-router-dom';
 

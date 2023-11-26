bfae956ae969b853356f5024387e21736b8eff04
Santiago Fleitas <sansfleitas@gmail.com>
2023-11-26 14:30:08 -0300

    Add Unit Testing Validation to pipeline

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -3,8 +3,7 @@ import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
 
 const server = setupServer(...handlers)
-server.events.on('request:start', ({ request }) => {
-  console.log('MSW intercepted:', request.method, request.url)
-})
+server.events.on('request:start', () => {
+});
 
 export { server }
